a75a3230dda66de7c5c7759395653c8774c7675e
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-11-28 01:58:15 +0530

    Adaptor part

---
+++ app/src/main/java/com/sala/recyclerview/Adaptor.java
@@ -0,0 +1,53 @@
+package com.sala.recyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+public class Adaptor extends RecyclerView.Adapter<Adaptor.carditem> {
+    Context context;
+
+    public Adaptor(Context context){
+        this.context = context;
+    }
+    public static class carditem extends RecyclerView.ViewHolder{
+
+        CardView cardView;
+        ImageView imageView;
+
+        public carditem(@NonNull View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView.findViewById(R.id.card);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public carditem onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_card,parent,false); //inflate the layout
+        //create the instance
+        carditem ci = new carditem(view);
+        return ci;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull carditem holder, int position) {
+        Picasso.get().load("https://cataas.com/cat?width=500").into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+
+}
--- app/src/main/java/com/sala/recyclerview/MainActivity.java
+++ app/src/main/java/com/sala/recyclerview/MainActivity.java
@@ -7,13 +7,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView rv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rv = (RecyclerView)findViewById(R.id.recyclerView);
+        RecyclerView rv = (RecyclerView) findViewById(R.id.recyclerView);
         rv.setHasFixedSize(true);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rv.setLayoutManager(llm);
