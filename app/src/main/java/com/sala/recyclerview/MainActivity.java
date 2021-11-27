ce3d998e8f660979c9d0896d3f874df043dd73cb
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-11-28 00:16:03 +0530

    Initial Kinda

---
+++ app/src/main/java/com/sala/recyclerview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sala.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView rv = (RecyclerView)findViewById(R.id.recyclerView);
+        rv.setHasFixedSize(true);
+    }
+}
