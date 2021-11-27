87f3bb74a2eed15eb559c41842ad3f9333fd2343
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-11-28 00:57:39 +0530

    Add recyclerview and LinearLayout Manager

--- app/src/main/java/com/sala/recyclerview/MainActivity.java
+++ app/src/main/java/com/sala/recyclerview/MainActivity.java
@@ -1,18 +1,21 @@
 package com.sala.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView rv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView rv = (RecyclerView)findViewById(R.id.recyclerView);
+        rv = (RecyclerView)findViewById(R.id.recyclerView);
         rv.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
     }
 }
